6f4e722d0ab3ede5659f0de182b0996cb1e070b7
Carl Gallardo <carlito1234121345@gmail.com>
2025-10-23 11:52:28 +0000

    please check codebase and clean not needed and irrelevant codes and file

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,8 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({
+    apiKey: '828556004852d02d939e7bc560f3ad14bafae57b'
+  })],
   model: 'googleai/gemini-2.5-flash',
 });
